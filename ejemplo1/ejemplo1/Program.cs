396f158846682d0e3c1ab817cf6162f6255ad977
chuyano <spawner.astral@hotmail.com>
2015-09-10 23:44:28 -0500

    segunda parte

--- ejemplo1/ejemplo1/Program.cs
+++ ejemplo1/ejemplo1/Program.cs
@@ -11,7 +11,7 @@ namespace ejemplo1
         static void Main(string[] args)
         {
             Console.WriteLine("hello world");
-
+            Console.WriteLine("hello");
             Console.ReadKey(true);
         }
     }
